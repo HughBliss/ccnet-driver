261b7dd823288ab519f2b6f75c69ef28cbe4f83c
Alexey Korzhavin <hughwols@gmail.com>
2024-07-18 21:04:47 +0500

    debug

--- example/example.ts
+++ example/example.ts
@@ -2,7 +2,7 @@ import { CCNET, DEVICE_TYPE } from '../src'
 
 (async () => {
   using ccnet = new CCNET({
-    path: '/dev/ttys021',
+    path: 'COM2',
     deviceType: DEVICE_TYPE.BILL_VALIDATOR,
     isDebugMode: true,
     timeout: 10000
